4136ce6b100ffe23beb65bae457c2027fc7b2f3e
GT_0 <riverasantospedroangel@gmail.com>
2025-04-08 14:54:24 -0500

    Add files via upload

---
+++ js/new_btn.js
@@ -0,0 +1,217 @@
+
+function BaraOpcions() {
+        const options1 = 
+`No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+Gana x2 (4000)  
+No pasa nada
+Gana x3 (6000)  
+Gana x5 (10,000)  
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+Gana x2 (4000)  
+Gana x3 (6000)  
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+No pasa nada
+Gana x5 (10,000)  
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+Gana x1.5 (3000)  
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x3 (6000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+No pasa nada
+Gana x5 (10,000)  
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+Gana x1.5 (3000)  
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x3 (6000)  
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+Gana x2 (4000)  
+No pasa nada
+Gana x3 (6000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x5 (10,000)  
+Gana x1.5 (3000)  
+No pasa nada
+Gana x1.5 (3000)
+Gana x1.5 (3000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+Gana x3 (6000)  
+No pasa nada
+No pasa nada
+No pasa nada
+Gana x2 (4000)  
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)  
+No pasa nada
+No pasa nada
+Gana x5 (10,000)
+No pasa nada
+Gana x1.5 (3000)
+No pasa nada
+Gana x3 (6000)
+No pasa nada
+No pasa nada
+Gana x1.5 (3000)
+No pasa nada
+Gana x2 (4000)
+No pasa nada
+Gana x1.5 (3000)
+No pasa nada
+Gana x3 (6000)
+Gana x2 (4000)
+No pasa nada
+No pasa nada`;
+        document.getElementById('ListaElementos').value = options1;
+        leerElementos();
+    }
+
+function MulticastOpcion() {
+        const options2 = 
+`NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+NO BASHEASTE
+BASH`;
+        document.getElementById('ListaElementos').value = options2;
+        leerElementos();
+    }
